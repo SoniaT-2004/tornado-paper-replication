24a3d6bf9d01010137fda805ace469ea992f5467
Sonia Seaweed <sonia.tao@hotmail.com>
2026-01-07 14:30:53 -0500

    Update download_small_dune_csv.py

--- dune_csv_download_helper/download_small_dune_csv.py
+++ dune_csv_download_helper/download_small_dune_csv.py
@@ -1,21 +1,20 @@
 import requests
 import os
 
-# === 1. CONFIGURATION ===
-API_KEY = "eD33FIunRML2oOozBVbMlnTu9nDSIVSo"   # API key
-QUERY_ID = 6222257              # Dune query ID
+API_KEY = "" # Your API key
+QUERY_ID = 6222257 # Dune query ID
 OUTPUT_FILE = f"untransferred.csv"
-LIMIT = 5000                    # Adjust if your result set > 1000 rows
+LIMIT = 5000 # Adjust this as needed
 OFFSET = 0
 
-# === 2. MAKE REQUEST ===
+# request
 url = f"https://api.dune.com/api/v1/query/{QUERY_ID}/results/csv?limit={LIMIT}&offset={OFFSET}"
 headers = {"X-Dune-API-Key": API_KEY}
 
 print("Fetching CSV data from Dune ...")
 response = requests.get(url, headers=headers)
 
-# === 3. CHECK & SAVE ===
+# save
 if response.status_code == 200:
     with open(OUTPUT_FILE, "wb") as f:
         f.write(response.content)

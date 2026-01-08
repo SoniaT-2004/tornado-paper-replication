abc853b8f23c16ad3591de004c666df5862074e4
Sonia Seaweed <sonia.tao@hotmail.com>
2026-01-07 21:20:35 -0500

    Update mixer_trend.R

--- analysis/mixer_trend.R
+++ analysis/mixer_trend.R
@@ -1,16 +1,14 @@
-# Load libraries
 library(tidyverse)
 library(lubridate)
 library(broom)
 
-# prepare
 df <- read_csv("/Users/soniatao/Desktop/Tornado/mixers/mixer_history_old/mixer_sum.csv")
 
 df <- df %>%
   mutate(day = ymd(day)) %>%
   arrange(day)
 
-# Summarize daily total transaction count (ETH + ERC20 if desired)
+# Summarize daily total transaction count
 df_daily <- df %>%
   mutate(total_tx = eth_tx_count + erc20_tx_count) %>%
   group_by(day) %>%
@@ -18,23 +16,20 @@ df_daily <- df %>%
   filter(between(day, as.Date("2020-01-01"), as.Date("2025-06-01"))) %>%
   summarise(daily_tx = sum(total_tx, na.rm = TRUE))
 
-# Define sanction date and add time variables
 sanction_date <- as.Date("2022-08-08")
 
 df_daily <- df_daily %>%
   mutate(
-    t = as.numeric(day - min(day)) + 1,             # running time index
-    post = if_else(day >= sanction_date, 1, 0),    # post dummy
+    t = as.numeric(day - min(day)) + 1, # running time index
+    post = if_else(day >= sanction_date, 1, 0), # post dummy
     t_after = if_else(post == 1, t - which(day == sanction_date)[1], 0)
   )
 
-
-# smooth series (30-day moving average)
+# smooth series (7-day moving average)
 df_daily <- df_daily %>%
   mutate(daily_tx_ma7 = zoo::rollmean(daily_tx, 7, fill = NA, align = "right"))
 
-# model with delayed effect
-# use a lag window (e.g., 7 days) to allow gradual change
+# use a lag window to allow gradual change
 df_daily <- df_daily %>%
   mutate(days_since = as.numeric(day - sanction_date)) %>%
   mutate(post_ramp = pmax(days_since, 0))  # increases linearly after event
@@ -53,4 +48,3 @@ ggplot(df_daily, aes(x = day, y = daily_tx_ma7)) +
     x = "", y = ""
   ) +
   theme_minimal()
-
